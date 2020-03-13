eeeb9236782955c7184303b939a5c04f4a6bf1c7
ibnbattuta <yoyox98@gmail.com>
2020-03-13 20:13:17 +0100

    added fabrik tests

--- src/main/java/fabrikexpression/ExprFactory.java
+++ src/main/java/fabrikexpression/ExprFactory.java
@@ -14,28 +14,43 @@ public class ExprFactory {
     }
 
     public Expression makeLeaf(String type, Object value) throws Exception {
-        Class<?> c = Class.forName("expression." + type);
-        Expression e = (Expression) c.getDeclaredConstructor().newInstance();
-        if(e instanceof Litteral) {
-            ((Litteral)e).set((String)value);
-        } else if (e instanceof Entier) {
-            ((Entier)e).set((Integer)value);
-        }else if (e instanceof Booleen) {
-            ((Booleen)e).set((Boolean)value);
+        try {
+            Class<?> c = Class.forName("expression." + type);
+            Expression e = (Expression) c.getDeclaredConstructor().newInstance();
+            if (e instanceof Litteral) {
+                if(!value.getClass().getName().equals("java.lang.String"))
+                     throw new IncorrectTypageException(type,"String");
+                ((Litteral) e).set((String) value);
+            } else if (e instanceof Entier) {
+                if(!value.getClass().getName().equals("java.lang.Integer"))
+                    throw new IncorrectTypageException(type,"Integer");
+                ((Entier) e).set((Integer) value);
+            } else if (e instanceof Booleen) {
+                if(!value.getClass().getName().equals("java.lang.Boolean"))
+                    throw new IncorrectTypageException(type,"Boolean");
+                ((Booleen) e).set((Boolean) value);
+            }
+            return e;
+        }catch (ClassNotFoundException ex){
+            throw new NonExistantTypeException(type);
+        }catch (IncorrectTypageException ex){
+            throw ex;
         }
-        return e;
     }
 
     public Expression makeNode(String type, Expression... children) throws Exception {
-
-        Class <?> c;
-        NonTerminal e;
-        c = Class.forName("expression." + type);
-        e = (NonTerminal) c.getDeclaredConstructor().newInstance();
-        for (Expression child : children) {
-            e.set(child);
+        try {
+            Class<?> c;
+            NonTerminal e;
+            c = Class.forName("expression." + type);
+            e = (NonTerminal) c.getDeclaredConstructor().newInstance();
+            for (Expression child : children) {
+                e.set(child);
+            }
+            return e;
+        }catch (ClassNotFoundException ex){
+                throw new NonExistantTypeException(type);
         }
-        return e;
     }
 
     private static ExprFactory instance;
--- src/main/java/fabrikexpression/NonExistantTypeException.java
+++ src/main/java/fabrikexpression/NonExistantTypeException.java
@@ -1,15 +1,18 @@
 package fabrikexpression;
 
 
-import java.util.ArrayList;
-
 public class NonExistantTypeException extends Exception {
     private String type;
-    public NonExistantTypeException(String  type) {
+    public NonExistantTypeException(String type) {
         this.type = type;
     }
 
     public String getType() {
         return this.type;
+    }
+
+    @Override
+    public String toString() {
+        return "Class '" + type + "' does not exist";
     }
 }
